88cfa360994f2bf62bb80f50e81dd3c866c86bae
Trim21 <trim21.me@gmail.com>
2024-10-01 10:31:38 +0800

    add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default tseslint.config({ ignores: ['dist/**'] }, eslint.configs.recommended, ...tseslint.configs.recommended, {
-  rules: { '@typescript-eslint/no-explicit-any': 'off' },
-})
+export default tseslint.config(
+  { ignores: ['dist/**'] },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: { '@typescript-eslint/no-explicit-any': 'off' },
+  },
+  eslintConfigPrettier,
+)
